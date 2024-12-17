ecba028a86cd8ef49744187acbfeb3f7d78447aa
Parsa Sedigh <parsa.sedigh78@gmail.com>
2024-12-17 23:07:37 +0330

    feat: watch section 19

--- app/app_test.go
+++ app/app_test.go
@@ -12,24 +12,30 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
+// call the handler that we wanna test, DIRECTLY without setting up a server
 func TestHome(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
+
 	app.Home(w, r)
 
 	resp := w.Result()
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		t.Errorf("ioutil.ReadAll() err = %s; want nil", err)
 	}
+
 	got := string(body)
 	want := "<h1>Welcome!</h1>"
+
 	if got != want {
 		t.Errorf("GET / = %s; want %s", got, want)
 	}
 }
 
+// call the handler that we wanna test, using the test server that we have set up
 func TestApp_v1(t *testing.T) {
 	server := httptest.NewServer(&app.Server{})
 	defer server.Close()
@@ -53,8 +59,28 @@ func TestApp_v1(t *testing.T) {
 	}
 }
 
+// approach 1: instead of having a "signed in client", we can create a "signed in req" every time we wanna make a req. But this approach has the
+// drawback of requiring us specifying a hard-coded cookie session and value, which sometimes we can't have.
+func signedInRequest(t *testing.T, method, target string, body io.Reader) *http.Request {
+	req, err := http.NewRequest(method, target, body)
+	if err != nil {
+		t.Fatalf("http.NewRequest() err = %s; want nil", err)
+	}
+
+	req.AddCookie(&http.Cookie{
+		Name:  "session",
+		Value: "fake_session_token",
+	})
+
+	return req
+}
+
+// approach 2: instead of using a "signed in req" every time, we can create a "signed in client".
+// Note: signedInClient returns a client that has called the login endpoint and have got a session cookie set in it's cookie jar, so that
+// we can make future AUTHENTICATED reqs
 func signedInClient(t *testing.T, baseURL string) *http.Client {
-	// Our cookiejar will keep and set cookies for us between requests.
+	// Our cookiejar will keep and set cookies for us between requests. It stores the cookies that the server sets for us and it's gonna
+	// set those cookies on every req. So every req that we make with client.Do(), is gonna set those cookies.
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
 		t.Fatalf("cookejar.New() err = %s; want nil", err)
@@ -70,11 +96,14 @@ func signedInClient(t *testing.T, baseURL string) *http.Client {
 	if err != nil {
 		t.Fatalf("NewRequest() err = %s; want nil", err)
 	}
+
 	_, err = client.Do(req)
 	if err != nil {
 		t.Fatalf("POST /login err = %s; want nil", err)
 	}
+
 	t.Logf("Cookies: %v", client.Jar.Cookies(req.URL))
+
 	return client
 }
 
@@ -82,28 +111,20 @@ type headerClient struct {
 	headers map[string]string
 }
 
+/* sets the headers that are in headerClient.headers field, so we don't have to set them everytime in our tests. */
 func (hc headerClient) Get(url string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
+
 	for hk, hv := range hc.headers {
 		req.Header.Set(hk, hv)
 	}
-	client := http.Client{}
-	return client.Do(req)
-}
 
-func signedInRequest(t *testing.T, method, target string, body io.Reader) *http.Request {
-	req, err := http.NewRequest(method, target, body)
-	if err != nil {
-		t.Fatalf("http.NewRequest() err = %s; want nil", err)
-	}
-	req.AddCookie(&http.Cookie{
-		Name:  "session",
-		Value: "fake_session_token",
-	})
-	return req
+	client := http.Client{}
+
+	return client.Do(req)
 }
 
 func TestApp_v2(t *testing.T) {
@@ -112,24 +133,32 @@ func TestApp_v2(t *testing.T) {
 
 	t.Run("custom built request", func(t *testing.T) {
 		t.Log(server.URL)
+
 		req := signedInRequest(t, http.MethodGet, server.URL+"/admin", nil)
+
 		var client http.Client
+
 		resp, err := client.Do(req)
 		if err != nil {
 			t.Fatalf("GET /admin err = %s; want nil", err)
 		}
 		defer resp.Body.Close()
+
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			t.Errorf("ioutil.ReadAll() err = %s; want nil", err)
 		}
+
 		got := string(body)
 		want := "<h1>Welcome to the admin page!</h1>"
+
 		if got != want {
 			t.Errorf("GET /admin = %s; want %s", got, want)
 		}
 	})
 
+	/* This test is saying: if we go to /admin, we should get 200 since we have the cookie in jar. But it doesn't set the auth header,
+	so we should get a 403.*/
 	t.Run("cookie based auth", func(t *testing.T) {
 		client := signedInClient(t, server.URL)
 		res, err := client.Get(server.URL + "/admin")
@@ -157,13 +186,17 @@ func TestApp_v2(t *testing.T) {
 		if err != nil {
 			t.Errorf("GET /admin err = %s; want nil", err)
 		}
+
 		if res.StatusCode != 403 {
 			t.Errorf("GET /admin code = %d; want %d", res.StatusCode, 403)
 		}
+
 		res, err = client.Get(server.URL + "/header-admin")
 		if err != nil {
 			t.Errorf("GET /header-admin err = %s; want nil", err)
 		}
+
+		// we do have the correct auth headers, so we expect a 200, otherwise fail the test.
 		if res.StatusCode != 200 {
 			t.Errorf("GET /header-admin code = %d; want %d", res.StatusCode, 200)
 		}
--- pamphlets/section-19-Testing-with-HTTP.md
+++ pamphlets/section-19-Testing-with-HTTP.md
@@ -36,3 +36,31 @@ But if you wanna test e2e and actually hit the server, use httptest.NewServer(),
 things happen as expected.
 
 ## 077 Building HTTP helpers
+Look at `app` folder.
+
+For authentication, in some apis, our server requires cookie based auth and also header basked auth.
+
+Now in our tests, to avoid repetitive code and also avoid hitting the login endpoint in every test, we have 2 approaches:
+- custom client. We can impl clients that do auth based on cookies or headers.
+- custom req
+
+There's one approach for header based.
+
+We can also have helpers and http clients that automatically figure out if we're an admin user and sign you as an admin. We can have
+helpers that sign you as a user with limited privileges to test how that works. 
+
+We can do e2e tests by creating these helpers and clients.
+
+Let's say we have multiple pages that require the client to be authenticated. We don't want to write code to get authenticated by
+going to login page, get a cookie, store that cookie in memory and so when we make a req, it would add that cookie to the req. That would
+be hard to do.
+
+Instead, we wanna create a client and a req that is authenticated. The simplest way to create a req is in `signedInRequest()` func.
+
+Note: One drawback of `signedInRequest` is it requires us to know a session. If we didn't have a hard-coded session token,
+it could be hard to do with this approach.
+We can't actually sign in like a real user would. That makes`signedInRequest` a bit less useful and not realistic.
+So we want a way to actually hit the server's login page and get a cookie and set that cookie in the cookie jar of our custom client.
+So that client will keep that info and it allows us to continue acting as an authenticated user(client).
+
+Instead of using a default http.Client, we create a custom client in `signedInClient` func.
